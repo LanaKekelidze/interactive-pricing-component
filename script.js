650f82bbd5a3bf546b5639482c16559f58f48413
LanaKekelidze <104271819+LanaKekelidze@users.noreply.github.com>
2023-06-01 20:12:10 +0400

    update js

--- script.js
+++ script.js
@@ -1,88 +1,32 @@
-let myCheckbox = document.querySelector(".switch input"),
-    duration = document.querySelector(".month"),
-    price = document.querySelector(".price .pricefix"),
-    switcher = document.querySelector(".switch"),
-    range = document.querySelector(".myrange");
+// Get DOM elements
+const rangeSlider = document.querySelector('.myrange');
+const priceElement = document.querySelector('.priecefix');
+const durationElement = document.querySelector('.month');
+const billingSwitch = document.querySelector('.switch input');
 
-// generate price on switch
-switcher.onclick = function(){
+// Pricing data
+const prices = [8, 12, 16, 24, 36]; // Monthly prices
+const discounts = [0, 25, 25, 25, 25]; // Discount percentages for yearly billing
 
+// Update pricing based on slider value and billing option
+function updatePricing() {
+  const sliderValue = rangeSlider.value;
+  const price = prices[sliderValue - 1];
+  const discount = discounts[sliderValue - 1];
 
-
-    if(myCheckbox.checked == true){
-
-        let priceInt = parseInt(price.textContent);
-        price.textContent = `${(priceInt - (priceInt * .25)) * 12}`;
-        duration.textContent = `/year`;
-
-    }else{
-
-        refreshPrices();
-        duration.textContent = `/month`;
-    }
+  if (billingSwitch.checked) {
+    // Yearly billing
+    const discountedPrice = price - (price * discount) / 100;
+    priceElement.textContent = discountedPrice.toFixed(2);
+    durationElement.textContent = '/year';
+  } else {
+    // Monthly billing
+    priceElement.textContent = price.toFixed(2);
+    durationElement.textContent = '/month';
+  }
 }
 
-// generate price on range
-let pageViews = document.querySelector(".page-views span");
-range.addEventListener('input', refreshPrices);
+// Event listeners
+rangeSlider.addEventListener('input', updatePricing);
+billingSwitch.addEventListener('change', updatePricing);
 
-function refreshPrices(){
-
-    if(range.value == 1){
-
-        if(myCheckbox.checked == true){
-            price.textContent = `${(8 - (8 * .25)) * 12}`;
-        }else{
-            price.textContent = 8;
-        }
-
-        pageViews.textContent = "10k";
-
-    } 
-
-    if(range.value == 2){
-
-        if(myCheckbox.checked == true){
-            price.textContent = 108;
-        }else{
-            price.textContent = 12;
-        }
-
-        pageViews.textContent = "50k";
-    }
-
-    if(range.value == 3){
-
-        if(myCheckbox.checked == true){
-            price.textContent = 144;
-        }else{
-            price.textContent = 16;
-        }
-
-        pageViews.textContent = "100k";
-    }
-
-    if(range.value == 4){
-
-        if(myCheckbox.checked == true){
-            price.textContent = 216;
-        }else{
-            price.textContent = 24;
-        }
-
-        pageViews.textContent = "500k";
-
-    }
-
-    if(range.value == 5){
-
-        if(myCheckbox.checked == true){
-            price.textContent = 324;
-        }else{
-            price.textContent = 36;
-        }
-
-        pageViews.textContent = "1M";
-    }
-
-}
